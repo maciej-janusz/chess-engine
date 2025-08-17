a7eb9af3be0fe01756f7ea304ec075cc60777c04
Maciej Janusz <maciejjanusz@MacBook-Air-Maciej.local>
2025-08-17 17:46:18 +0200

    ui

--- main.cpp
+++ main.cpp
@@ -11,8 +11,10 @@ int main()
 {
     // 2bB4/N2k1N2/3P1P2/4p3/1R3p1P/Q6p/6p1/K3R3 w - - 0 1
     std::string fen;
+    std::cout << "Enter FEN: ";
     std::getline(std::cin, fen);
-    int depth = readInt();;
+    std::cout << "Enter depth: ";
+    int depth = readInt();
     findBestVariant(fen, depth);
     return 0;
 }
